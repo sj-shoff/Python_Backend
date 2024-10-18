c67f993ccab22c21eb7d45355f49651f10dbf0c3
sj-shoff <atryakhin0306@gmail.com>
2024-10-18 17:16:30 +0300

    modified:   short_linker/presentations/__pycache__/app.cpython-310.pyc 	modified:   short_linker/presentations/app.py

--- short_linker/presentations/app.py
+++ short_linker/presentations/app.py
@@ -3,12 +3,18 @@ from pydantic import BaseModel
 from services.short_link_service import ShortLinkService
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Сервис генерации коротких ссылок", 
+    description="Простенький сервис для создания коротких ссылок",
+    )
 short_link_service = ShortLinkService()
 
 
 @app.post("/health")
 def hello_world() -> str:
+    """
+    Тестовый эндпоинт
+    """
     return "ok"
 
 class PutLink(BaseModel):
@@ -17,6 +23,9 @@ class PutLink(BaseModel):
 
 @app.put("/link")
 def put_link(long_link: PutLink) -> PutLink:
+    """
+    Метод создания короткой ссылки по длинной ссылке
+    """
     short_link = short_link_service.put_link(long_link.link)
 
     return PutLink(link=short_link)
@@ -24,8 +33,15 @@ def put_link(long_link: PutLink) -> PutLink:
 
 @app.get("/short/{short_link}")
 def get_link(short_link: str = Path(...)) -> Response:
+    """
+    Метод переадресации с короткой ссылки на длинную
+    """
     long_link = short_link_service.get_link(short_link)
 
-    return Response(content=None, headers={"Location": "https://misis.ru"}, status_code=status.HTTP_301_MOVED_PERMANENTLY)
+    return Response(
+        content=None, 
+        headers={"Location": "https://misis.ru"}, 
+        status_code=status.HTTP_301_MOVED_PERMANENTLY,
+        )
 
 
